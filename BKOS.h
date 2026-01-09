1a49147f943ba7024024497e522f63c8595e0749
BrendanKoster86 <brendan.koster@gmail.com>
2026-01-09 09:07:35 +0100

    OTA test

--- BKOS.h
+++ BKOS.h
@@ -10,14 +10,14 @@
 #include "info.h"
 #include "ota.h"
 
-#define BKOS_VERSIE "4.T260109"
+#define BKOS_VERSIE "4.T260107"
 
 void BKOS_boot();
 
 unsigned int klok_getekend = 0;
 
 #if HARDWARE == 5
-  const char* firmwareUrl = "https://github.com/BrendanKoster86/BKOS4/blob/main/firmware/firmware5.ino.bin";
+  const char* firmwareUrl = "https://github.com/BrendanKoster86/BKOS4/blob/main/build/esp32.esp32.esp32s3/BKOS4.ino.bin";
   const char* versionUrl = "https://github.com/BrendanKoster86/BKOS4/blob/main/firmware/versie5.txt";
 #else
   const char* firmwareUrl = "https://github.com/BrendanKoster86/BKOS4/blob/main/firmware/firmware.ino.bin";
